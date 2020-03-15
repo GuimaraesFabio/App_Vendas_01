2beaab4d0c9a7b56de88c810995b1acd58bac29d
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-15 14:09:32 -0300

    Implementado o metodo findById nas classe ClienteService e ClienteResource

--- src/main/java/com/services/ClienteService.java
+++ src/main/java/com/services/ClienteService.java
@@ -1,12 +1,14 @@
 package com.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Cliente;
 import com.repositories.ClienteRepository;
+import com.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
@@ -16,5 +18,10 @@ public class ClienteService {
 
 	public List<Cliente> findAll() {
 		return _repository.findAll();
+	}
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = _repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
