53519ce2a37c71ee980ee5f5dda70a580aba3528
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-15 13:49:27 -0300

    Modificação na classe Cliente e DbService

--- src/main/java/com/services/DbService.java
+++ src/main/java/com/services/DbService.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.domain.Categoria;
 import com.domain.Cidade;
+import com.domain.Cliente;
+import com.domain.Endereco;
 import com.domain.Estado;
 import com.domain.Produto;
+import com.enums.TipoCliente;
 import com.repositories.CategoriaRepository;
 import com.repositories.CidadeRepository;
+import com.repositories.ClienteRepository;
+import com.repositories.EnderecoRepository;
 import com.repositories.EstadoRepository;
 import com.repositories.ProdutoRepository;
 
@@ -28,6 +33,12 @@ public class DbService {
 
 	@Autowired
 	private CidadeRepository _cidadeRepo;
+
+	@Autowired
+	private ClienteRepository _clienteRepo;
+
+	@Autowired
+	private EnderecoRepository _enderecoRepo;
 
 	public void InstanciateTestDataBase() {
 
@@ -61,6 +72,16 @@ public class DbService {
 
 		_estadoRepo.saveAll(Arrays.asList(est1, est2));
 		_cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria da Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		_clienteRepo.saveAll(Arrays.asList(cli1));
+		_enderecoRepo.saveAll(Arrays.asList(e1, e2));
 
 	}
 }
