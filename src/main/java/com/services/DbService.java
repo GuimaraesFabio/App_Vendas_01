4db3477515825bfa15fc37c26e547c27c7050f32
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-15 12:42:08 -0300

    Criado e implementado: Interfaces EstadoRepository e CidadeRepository

--- src/main/java/com/services/DbService.java
+++ src/main/java/com/services/DbService.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Categoria;
+import com.domain.Cidade;
+import com.domain.Estado;
 import com.domain.Produto;
 import com.repositories.CategoriaRepository;
+import com.repositories.CidadeRepository;
+import com.repositories.EstadoRepository;
 import com.repositories.ProdutoRepository;
 
 @Service
@@ -19,13 +23,19 @@ public class DbService {
 	@Autowired
 	private ProdutoRepository _produtoRepo;
 
+	@Autowired
+	private EstadoRepository _estadoRepo;
+
+	@Autowired
+	private CidadeRepository _cidadeRepo;
+
 	public void InstanciateTestDataBase() {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 
 		_categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -38,5 +48,19 @@ public class DbService {
 		p3.getCategorias().add(cat1);
 
 		_produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().add(c1);
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		_estadoRepo.saveAll(Arrays.asList(est1, est2));
+		_cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 }
