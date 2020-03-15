860334c70310eb0d57e2b82e5db52222a0408d7c
Fabio Guimaraes <guimaraes.familia2018@gmail.com>
2020-03-15 20:57:23 -0300

    Criado e implementado: Operações CRUD na classe ClienteService e ClienteResource

---
+++ src/main/java/com/dto/ClienteDto.java
@@ -0,0 +1,62 @@
+package com.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.domain.Cliente;
+
+public class ClienteDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	
+	@NotEmpty(message = "Campo obrigatorio")
+	@Length(min = 5, max = 120, message = "Tamanho do campo nome deve estar entre 5 a 120 caracteres")
+	private String nome;
+	@NotEmpty(message = "Campo obrigatorio")
+	@Email(message = "Email invalido")
+	private String email;
+	
+	public ClienteDto() {
+	}
+
+	public ClienteDto(Cliente obj) {
+		
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/services/ClienteService.java
+++ src/main/java/com/services/ClienteService.java
@@ -4,10 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.domain.Cidade;
 import com.domain.Cliente;
+import com.domain.Endereco;
+import com.dto.ClienteDto;
+import com.dto.ClienteNewDto;
+import com.enums.TipoCliente;
+import com.repositories.CidadeRepository;
 import com.repositories.ClienteRepository;
+import com.repositories.EnderecoRepository;
+import com.services.exceptions.DataIntegrityException;
 import com.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +29,12 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository _repository;
 
+	@Autowired
+	private CidadeRepository _cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository _enderecoRepository;
+
 	public List<Cliente> findAll() {
 		return _repository.findAll();
 	}
@@ -23,5 +42,65 @@ public class ClienteService {
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = _repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linePerPage, String direction, String orderBy) {
+		PageRequest pr = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return _repository.findAll(pr);
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		if (obj.getId() == null) {
+			_repository.save(obj);
+			_enderecoRepository.saveAll(obj.getEnderecos());
+		}
+		return obj;
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return _repository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			_repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("DataIntegrity error reference.");
+		}
+
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+
+		newObj.setNome(obj.getNome());
+	}
+
+	public Cliente fromDto(ClienteDto objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDto(ClienteNewDto objDto) {
+
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = _cidadeRepository.getOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+
+		cli.getTelefones().add(objDto.getTelefone1());
+		cli.getEnderecos().add(end);
+
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
 	}
 }
